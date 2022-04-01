2fdaff83235003345f469fab15872d6df93b6683
Aleksandra Byra <aleksandra.byra@aptiv.com>
2022-04-01 12:49:46 +0200

    [Exercise 3] Prime factors - 6th iteration

--- src/prime_factors.py
+++ src/prime_factors.py
@@ -2,6 +2,8 @@
 Tests:
     >>> prime_factors(12)
     [2, 2, 3]
+    >>> prime_factors(2347)
+    [2347]
     >>> prime_factors(0)
     Traceback (most recent call last):
         ...
@@ -23,11 +25,28 @@ Tests:
         ...
     ValueError
 """
+THE_LOWEST_FACTOR: int = 2
 
 
-def prime_factors(number: int):
+def prime_factors(number: int) -> list:
     if not isinstance(number, int):
         raise ValueError
-    if number < 2:
+    if number < THE_LOWEST_FACTOR:
         raise ValueError
-    return [2, 2, 3]
+
+    prime_factors_list: list = []
+    _add_flag: bool = True
+    factor: int = 2
+
+    while factor <= number:
+        if number % factor == 0:
+            number /= factor
+            prime_factors_list.append(factor)
+        else:
+            factor += 1
+
+    if len(prime_factors_list) == 0:
+        prime_factors_list.append(number)
+    return prime_factors_list
+
+
