d33e3ac5e386eeba0ea35ea3f2e338ee6754321e
Aleksandra Byra <aleksandra.byra@aptiv.com>
2022-04-06 15:01:37 +0200

    [Exercise 2.3] Roman to integer - iteration 2

--- src/roman_to_integer.py
+++ src/roman_to_integer.py
@@ -1,3 +1,10 @@
+from re import sub
+
+
 def roman_to_integer(roman_number: str):
     if not isinstance(roman_number, str):
         raise TypeError
+
+    allowed_roman_signs_regex = '[^IVXLCDM]+'
+    if roman_number != sub(allowed_roman_signs_regex, '', roman_number):
+        raise ValueError
--- tests/test_roman_to_integer.py
+++ tests/test_roman_to_integer.py
@@ -6,3 +6,8 @@ import pytest
 def test_int_input_data_raises_type_error():
     with pytest.raises(TypeError):
         roman_to_integer(13)
+
+
+def test_not_roman_signs_raises_value_error():
+    with pytest.raises(ValueError):
+        roman_to_integer('LXDMIXUHWDCS')
